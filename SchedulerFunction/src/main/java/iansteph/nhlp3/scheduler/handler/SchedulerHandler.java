fbe2f080f785c971dd0e2131ba1495069c695397
Ian Stephenson 🦈🏒 <ianp.stephenson@yahoo.com>
2019-08-05 18:38:28 -0700

    Added unit test for ScheduleHandler

--- SchedulerFunction/src/main/java/iansteph/nhlp3/scheduler/handler/SchedulerHandler.java
+++ SchedulerFunction/src/main/java/iansteph/nhlp3/scheduler/handler/SchedulerHandler.java
@@ -3,11 +3,20 @@ package iansteph.nhlp3.scheduler.handler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import iansteph.nhlp3.scheduler.client.NhlClient;
+import iansteph.nhlp3.scheduler.model.Date;
+import iansteph.nhlp3.scheduler.model.Game;
 import iansteph.nhlp3.scheduler.model.ScheduleResponse;
 import iansteph.nhlp3.scheduler.proxy.NhlProxy;
+import software.amazon.awssdk.services.eventbridge.EventBridgeClient;
+import software.amazon.awssdk.services.eventbridge.model.PutRuleRequest;
+import software.amazon.awssdk.services.eventbridge.model.RuleState;
 
 import java.time.LocalDate;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import static java.lang.String.format;
 
 /**
  * Handler for requests to Lambda function. This implements Lambda's RequestHandler interface which allows it to connect to Lambda.
@@ -17,8 +26,21 @@ import java.time.format.DateTimeFormatter;
  */
 public class SchedulerHandler implements RequestHandler<Object, Object> {
 
+    private EventBridgeClient eventBridgeClient;
+    private NhlProxy nhlProxy;
+
+    public SchedulerHandler(final NhlProxy nhlProxy, final EventBridgeClient eventBridgeClient) {
+        this.eventBridgeClient = eventBridgeClient;
+        this.nhlProxy = nhlProxy;
+    }
+
     public Object handleRequest(final Object input, final Context context) {
-        final NhlProxy nhlProxy = new NhlProxy(new NhlClient());
+        if (nhlProxy == null) {
+            nhlProxy = new NhlProxy(new NhlClient());
+        }
+        if (eventBridgeClient == null) {
+            eventBridgeClient = EventBridgeClient.create();
+        }
 
         final ScheduleResponse scheduleResponseForDate = nhlProxy.getScheduleForDate(LocalDate.parse("2018-01-09",
                 DateTimeFormatter.ISO_LOCAL_DATE));
@@ -28,6 +50,34 @@ public class SchedulerHandler implements RequestHandler<Object, Object> {
                 DateTimeFormatter.ISO_LOCAL_DATE), LocalDate.parse("2018-01-12", DateTimeFormatter.ISO_LOCAL_DATE));
         System.out.println(scheduleResponseForDateRange);
 
+        setEventProcessingForGames(scheduleResponseForDate.getDates());
+        setEventProcessingForGames(scheduleResponseForDateRange.getDates());
+
         return new Object();
+    }
+
+    private void setEventProcessingForGames(final List<Date> dates) {
+        dates.stream()
+                .map(Date::getGames)
+                .forEach((games -> games.forEach(this::setEventProcessingForGame)));
+    }
+
+    private void setEventProcessingForGame(final Game game) {
+        final PutRuleRequest putRuleRequest = PutRuleRequest.builder()
+                .description(format("Event Rule triggering every minute invoking the play-by-play processor lambda for gameId: %s",
+                        game.getGamePk()))
+                .name(format("GameId:%s", game.getGamePk()))
+                .scheduleExpression(createCronExpressionForPutRuleRequest(game))
+                .state(RuleState.ENABLED)
+                .build();
+        eventBridgeClient.putRule(putRuleRequest);
+    }
+
+    private String createCronExpressionForPutRuleRequest(final Game game) {
+        final ZonedDateTime date = game.getGameDate();
+        final int dayOfMonth = date.getDayOfMonth();
+        final int month = date.getMonthValue();
+        final int year = date.getYear();
+        return format("cron(0/60 * * %s %s %s)", dayOfMonth, month, year);
     }
 }
--- SchedulerFunction/src/test/java/iansteph/nhlp3/scheduler/handler/SchedulerHandlerTest.java
+++ SchedulerFunction/src/test/java/iansteph/nhlp3/scheduler/handler/SchedulerHandlerTest.java
@@ -1,14 +1,60 @@
 package iansteph.nhlp3.scheduler.handler;
 
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import com.google.common.collect.ImmutableList;
+import iansteph.nhlp3.scheduler.client.NhlClient;
+import iansteph.nhlp3.scheduler.model.Date;
+import iansteph.nhlp3.scheduler.model.Game;
+import iansteph.nhlp3.scheduler.model.ScheduleResponse;
+import iansteph.nhlp3.scheduler.proxy.NhlProxy;
+import org.junit.Before;
 import org.junit.Test;
+import software.amazon.awssdk.services.eventbridge.EventBridgeClient;
+import software.amazon.awssdk.services.eventbridge.model.PutRuleRequest;
+import software.amazon.awssdk.services.eventbridge.model.PutRuleResponse;
+
+import java.time.ZonedDateTime;
 
 public class SchedulerHandlerTest {
 
+    private NhlClient mockNhlClient = mock(NhlClient.class);
+    private NhlProxy mockNhlProxy = new NhlProxy(mockNhlClient);
+    private EventBridgeClient mockEventBridgeClient = mock(EventBridgeClient.class);
+
+    @Before
+    public void setMockNhlClient() {
+        final Game game1 = new Game();
+        game1.setGameDate(ZonedDateTime.now());
+        game1.setGamePk(0);
+
+        final Game game2 = new Game();
+        game2.setGamePk(0);
+        game2.setGameDate(ZonedDateTime.now());
+
+        final Date date1 = new Date();
+        date1.setGames(ImmutableList.of(game1));
+
+        final Date date2 = new Date();
+        date2.setGames(ImmutableList.of(game2));
+
+        final ScheduleResponse scheduleResponse = new ScheduleResponse();
+        scheduleResponse.setDates(ImmutableList.of(date1));
+
+        final ScheduleResponse scheduleResponseForDateRange = new ScheduleResponse();
+        scheduleResponseForDateRange.setDates(ImmutableList.of(date1, date2));
+
+        when(mockNhlClient.getScheduleForDate(any())).thenReturn(scheduleResponse);
+        when(mockNhlClient.getScheduleForDateRange(any(), any())).thenReturn(scheduleResponseForDateRange);
+        when(mockEventBridgeClient.putRule(any(PutRuleRequest.class))).thenReturn(PutRuleResponse.builder().build());
+    }
+
     @Test
     public void successfulResponse() {
-        final SchedulerHandler schedulerHandler = new SchedulerHandler();
+        final SchedulerHandler schedulerHandler = new SchedulerHandler(mockNhlProxy, mockEventBridgeClient);
         final Object result = schedulerHandler.handleRequest(null, null);
         assertNotNull(result);
     }
